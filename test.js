b94639fe0ad6de1dc645b42fd15d44eb821f5b3a
Semih Bekki <semih.bekki@ghotmail.com>
2022-05-24 14:32:01 +0200

    01 Intro done

---
+++ test.js
@@ -0,0 +1,57 @@
+/* console.log("It works !!!");
+ */
+
+/* const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+*/
+
+/*************************/
+/*************************/
+
+/* EXERCICE 1.1 */
+/* let name = "Semih";
+console.log(name); */
+
+
+/* EXERCICE 1.2 */
+/* let name = "Dumbledore";
+let firstName = "Albert";
+let city = "Pouddlard";
+
+console.log(`Your name is ${firstName} ${name} and you live in ${city}.`) */
+
+
+/* EXERCICE 1.3 */
+/* const readlineSync = require("readline-sync");
+
+let firstName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + firstName); */
+
+
+/* EXERCICE 1.4 */
+/* const readlineSync = require("readline-sync");
+
+let name = readlineSync.question('Can you give me your name please?');
+let firstName = readlineSync.question('Can you give me your first name please?');
+let city = readlineSync.question('Can you give me your city please?');
+
+console.log(`Your name is ${firstName} ${name} and you live in ${city}.`); */
+
+
+/* EXERICE 1.5 */
+/* const readlineSync = require("readline-sync");
+
+let number1 = readlineSync.question('Can you give me a decimal number please?');
+let number2 = readlineSync.question('Can you give me another decimal number please?');
+
+console.log(parseInt(number1) + parseInt(number2)); */
+
+/* EXERCICE 1.6 */
+const readlineSync = require("readline-sync");
+
+let number1 = readlineSync.question('Can you give me a decimal number please?');
+let number2 = readlineSync.question('Can you give me another decimal number please?');
+
+console.log(parseInt(number1 % number2));
