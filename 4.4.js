fd20d8c63791851a4b5f8aa032b8d382fa2f30ac
Semih Bk <97962049+SemihBk@users.noreply.github.com>
2022-05-26 23:15:46 +0200

    Update 4.4.js

--- 4.4.js
+++ 4.4.js
@@ -1,8 +1,62 @@
-let array1 = [0, 2, 4, 6, 8];
+const readlineSync = require("readline-sync");
 
-function average(arr) {
-  arr.reduce((a, b) => a + b, 0) / arr.length;
-  return;
+let n = readlineSync.question('Can you give me a number ?');
+
+let array1 = [];
+let sum = 0;
+
+
+
+function rand10(max) {
+  return (Math.floor(Math.random() * max));
 }
 
-console.log(average(array1));
+function multiRand(a, tab) {
+  for (i = 0; i < a; i++) {
+    tab.push(rand10(11));
+  }
+  return tab
+}
+
+let array = multiRand(n, array1)
+
+console.log(array);
+
+
+/***** */
+
+
+
+function average(arr) {
+  let i = 0;
+  let sum = 0;
+  let ArrayLen = arr.length;
+  while (i < ArrayLen) {
+      sum = sum + arr[i++];
+}
+  return sum / ArrayLen;
+}
+
+let sumAverage = average(array);
+/* 
+console.log(sumAverage); */
+
+
+/* var myArray = [1, 5, 2, 3, 7];
+var a = ArrayAvg(myArray);
+console.log(a) */
+
+function min(arr) {
+  return Math.min(...arr);
+}
+
+let minNum = min(array);
+
+
+function max(arr) {
+  return Math.max(...arr);
+}
+
+let maxNum = max(array);
+
+console.log(`La moyenne de l'array est de ${sumAverage}, le nombre minimum de l'array est de ${minNum} et son nombre maximum est de ${maxNum}`);
